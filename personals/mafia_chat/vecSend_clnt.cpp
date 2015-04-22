e9ea5c3f4d0d1f5d1b0f9c02305a6630bf977c92
woodok <newwoodok@gmail.com>
2015-04-22 17:54:12 +0900

    vector sending practice ver_0_1

---
+++ personals/mafia_chat/vecSend_clnt.cpp
@@ -0,0 +1,10 @@
+/*
+ * vec_Send_clnt.cpp
+ *
+ *  Created on: Apr 22, 2015
+ *      Author: nubuntu
+ */
+
+
+
+
---
+++ personals/mafia_chat/vecSend_serv.cpp
@@ -0,0 +1,10 @@
+/*
+ * vecSend_serv.cpp
+ *
+ *  Created on: Apr 22, 2015
+ *      Author: nubuntu
+ */
+
+
+
+
